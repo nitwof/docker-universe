59474e071a9ac99f017f4ec3d95c39eda33e76ab
Dmitry Sinelnikov <dsinelnikov96@gmail.com>
2017-05-22 17:08:38 +0300

    In development

--- docker-universe.sh
+++ docker-universe.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# GSUP_IMAGE="registry:5000/group-supervisor"
-GSUP_IMAGE="docker:stable"
+GSUP_IMAGE="registry:5000/group-supervisor"
 
 function start () {
   docker swarm init
@@ -16,7 +15,21 @@ function start () {
 
 function join () {
   host=$1
-
+  group = get "$host/node/group"
+  if group != null {
+    token = get "$host/node/swarm_token"
+    docker swarm join --token token $host
+  } else {
+    docker swarm init
+    docker pull $GSUP_IMAGE
+    docker service create --mode global --name group_storage redis
+    docker service create --mode global -p 10000 \
+                          --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+                          --env STORAGE_URL="redis://group_storage:6379/0" \
+                          --env HOSTNAME=$(hostname) \
+                          --env JOIN=$host \
+                          --name group_supervisor $GSUP_IMAGE
+  }
 }
 
 case "$1" in
